d92d506189fff0a2835eb987140d8e48055c35e6
Maison16 <mateusz.izdebski125@gmail.com>
2024-04-02 15:29:28 +0200

    dodanie obsługi sygnałów oraz przejścia w stan spoczynku naszego Demona

--- demon.c
+++ demon.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <errno.h> // do obłsugi błędów
+#include <errno.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -9,293 +9,235 @@
 #include <dirent.h>
 #include <sys/mman.h>
 #include <utime.h>
-#define SMALL_FILE_SIZE 1024*1024
-#define STANDARD_SLEEP_TIME 300
+#include <signal.h>
 
-void Init(int argc,char* argv[]); //Inicjalizuje demona
-void CopyFile(const char* srcFile, const char* dstFile); //Kopiuje pliki
-void SynchroniseDirectories(const char* sourceDir, const char* destinationDir, int isRecursive); // Synchronizuje katalogii
-int ChangeTime(const char* input); //Sprawdza czy talbica char jest liczbą
-int IsDirectoryExists(const char *path); //Sprawdza czy path odnosi się do katalogu
-void WriteErrorAtributes(const char *programName); // Wypisuje błąd z parametrami
-int ChangeSize(const char* input); //Sprawdza czy talbica char jest liczbą
-int sleepTime;
-int sizeFile;
+#define SIGALRM 14
+#define SIGUSR1 10
+#define SMALL_FILE_SIZE 1024*1024
+#define STANDARD_SLEEP_TIME 5//300 do testow na razie co 5 sekund zamiast 5 minut
+
+void Init(int argc, char* argv[]);
+void CopyFile(const char* srcFile, const char* dstFile);
+void SynchroniseDirectories(const char* sourceDir, const char* destinationDir, int isRecursive);
+int ChangeTime(const char* input);
+int ChangeSize(const char* input);
+int IsDirectoryExists(const char *path);
+void WriteErrorAtributes(const char *programName);
+void SignalHandler(int sig);
+void AlarmHandler(int sig);
+
+char* sourceDir;
+char* destinationDir;
+
 int main(int argc, char* argv[])
 {
-	sleepTime = STANDARD_SLEEP_TIME;
-	sizeFile = SMALL_FILE_SIZE;
-	Init(argc, argv);
-	
+    printf("Demon Kopiujący Pliki został uruchomiony.\n");
+    // Ustawienie obsługi sygnałów
+    signal(SIGALRM, AlarmHandler);
+    signal(SIGUSR1, SignalHandler);
+    Init(argc, argv);
+    // Pobranie pid
+    pid_t pid = getpid();
+    alarm(STANDARD_SLEEP_TIME);
+    // Pętla nieskończona, aby program mógł otrzymywać sygnały
+    while (1) {
+         pause();
+    }
 
-	/*while(1)
-	{
-		sleep(sleepTime);
-		SynchroniseDirectories(argv[1], argv[2], 0);
-	}*/
-	printf("\nWykonało kopiowanie plików\n");
-
+    return 0;
 }
 
-
-//Inicjalizuje demona
-void Init(int argc,char* argv[])
-{
-	if(argc >= 3 && argc <= 6) //Sprawdzanie liczby parametrów
-	{
-		char* sourceDir = argv[1];
-		char* destinationDir = argv[2];
-
-		//Sprawdzanie poprawność przejścia do katalogów
-		if( IsDirectoryExists(sourceDir) || IsDirectoryExists(destinationDir) )
-		{
-			printf("\nNie można otworzyć katalogu/katalogów\n");
-			exit(1);
-		}
-
-		switch(argc) //Sprawdza poprawność podanych parametrów
-		{
-			// <src_dir> <dest_dir> 
-			case 3:
-				SynchroniseDirectories(argv[1], argv[2], 0);
-				break;
-			// <src_dir> <dest_dir> [sleepTime] 
-			// <src_dir> <dest_dir> [-R]
-			case 4: 
-				if(ChangeTime(argv[3])) //Wykonaj podstawowe zadanie z zmienionym czasie
-				{
-					SynchroniseDirectories(argv[1], argv[2], 0);
-				}
-				else if(strcmp(argv[3],"-R") == 0) //Wykonuje opcje -R
-				{
-					SynchroniseDirectories(argv[1], argv[2], 0);
-				}
-				else
-				{
-					WriteErrorAtributes(argv[0]);
-				}
-				break;
-			// <src_dir> <dest_dir> [sleepTime] [-R]
-			// <src_dir> <dest_dir> [-R] [sizeFile]
-			case 5: 
-				if(ChangeTime(argv[3]))
-				{}
-				else
-				{
-					if(strcmp(argv[3],"-R") == 0) 
-					{}
-					else
-					{
-						WriteErrorAtributes(argv[0]);
-					}
-					if(ChangeSize(argv[4])) 
-					{
-						SynchroniseDirectories(argv[1], argv[2], 1);	
-					}	
-					else
-					{
-						WriteErrorAtributes(argv[0]);
-					}
-				}
-				if(strcmp(argv[4],"-R") == 0) //Wykonuje opcje -R ale z zmienionym czasie
-				{	
-						SynchroniseDirectories(argv[1], argv[2], 1);	
-				}	
-				else
-				{
-					WriteErrorAtributes(argv[0]);
-				}
-				break;
-			// <src_dir> <dest_dir> [sleepTime] [-R] [sizeFile]
-			case 6:
-				if(ChangeTime(argv[3]))
-				{}
-				else
-				{
-					WriteErrorAtributes(argv[0]);
-				}
-				if(strcmp(argv[4],"-R") == 0) //Wykonuje opcje -R ale z zmienionym czasie
-				{}
-				else
-				{
-					WriteErrorAtributes(argv[0]);
-				}
-				if(ChangeSize(argv[5]))
-				{
-					SynchroniseDirectories(argv[1], argv[2], 1);	
-				}
-				else
-				{
-					WriteErrorAtributes(argv[0]);
-				}
-			default:
-				break;
-		}
-	}
-	else
-	{
-		printf("Zła liczba parametrów: %s <src_dir> <dest_dir> [sleepTime] [-R]\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
+void AlarmHandler(int sig) {
+    if (sig == SIGALRM) {
+        // Wyślij sygnał SIGUSR1
+        pid_t pid = getpid();
+        kill(pid, SIGUSR1);
+    alarm(STANDARD_SLEEP_TIME);
+ }
 }
 
-//Kopiuje pliki
-void CopyFile(const char* srcFile, const char* dstFile)
-{
-    	int fileToRead = open(srcFile, O_RDONLY);
-	struct stat stats; //Przechowuje informacje o pliku
-	fstat(fileToRead, &stats);
-	
-	if(stats.st_size <= sizeFile) //Kopiowanie dla małych plików
-	{
-	
-		//Wczytywanie
-		char buf[sizeFile];
-		int bytesNumber = read(fileToRead, buf, sizeFile);
-		
-		//Zapisywanie
-		int fileToSave = open(dstFile, O_WRONLY | O_CREAT | O_TRUNC, stats.st_mode);
-		write(fileToSave, buf, bytesNumber);
-		close(fileToSave);
-	}
-	else //Kopiowanie dla większych plików
-	{
-		//Mapowanie
-		void *srcPTR = mmap(NULL, stats.st_size, PROT_READ, MAP_PRIVATE, fileToRead, 0);
-		
-		//Zapisywanie
-		int fileToSave = open(dstFile, O_WRONLY | O_CREAT | O_TRUNC, stats.st_mode);
-		write(fileToSave, srcPTR, stats.st_size);
-		
-		//Usuwanie mapowania oraz zamykanie pliku
-       		munmap(srcPTR, stats.st_size);
-        	close(fileToSave);
-	}
-	close(fileToRead);
+void SignalHandler(int sig) {
+    if (sig == SIGUSR1) {
+        // Wykonaj synchronizację
+        SynchroniseDirectories(sourceDir, destinationDir, 0);
+	printf("Synchronizacja zakonczona.\n");
+    }
 }
 
-// Synchronizuje katalogii
-void SynchroniseDirectories(const char* sourceDir, const char* destinationDir, int isRecursive)
-{
-	DIR *srcDIR = opendir(sourceDir);
-	struct dirent *checkAll;
-	while ((checkAll = readdir(srcDIR)) != NULL) 
-	{
-		//Przeszukuje wszystkie pliku pomijając plik . - bieżący katalog oraz .. - nadrzędny
-		if (strcmp(checkAll->d_name, ".") == 0 || strcmp(checkAll->d_name, "..") == 0) 
-		{
-		    continue;
-        	}
+void Init(int argc, char* argv[]) {
+    if (argc >= 3 && argc <= 6) {
+        sourceDir = argv[1];
+        destinationDir = argv[2];
 
-        	
-        //Tworzenie scieżek do plików źródłowych i docelowych
-		char* srcFile = malloc((strlen(sourceDir)+strlen(checkAll->d_name)+2)*sizeof(char));
-		strcpy(srcFile, sourceDir);
-		strcat(srcFile, "/");
-		strcat(srcFile, checkAll->d_name);
-		
-		char* dstFile = malloc((strlen(destinationDir)+strlen(checkAll->d_name)+2)*sizeof(char));
-	  	strcpy(dstFile, destinationDir);
-		strcat(dstFile, "/");
-		strcat(dstFile, checkAll->d_name);
+        if (IsDirectoryExists(sourceDir) || IsDirectoryExists(destinationDir)) {
+            printf("\nNie można otworzyć katalogu/katalogów\n");
+            exit(1);
+        }
 
-		//Pobieranie właściwości o pliku źródłowym
-        	struct stat srcStats, dstStats;
-		stat(srcFile, &srcStats);
-		
-		//Jeżeli plik jest katalogiem oraz mamy opcje rekurencjii to wykonujemy też synchronizacje dla elementów w danym katalogu
-		if (S_ISDIR(srcStats.st_mode) && isRecursive == 1) 
-		{
-			// TODO: Tu implementuje rekurencje -R
-		} 
-		else if (S_ISREG(srcStats.st_mode)) 
-		{
-			//Jeżeli plik nie istnieje lub jest starszy dokonaj jego kopiowania
-			if (stat(dstFile, &dstStats) == -1 || srcStats.st_mtime > dstStats.st_mtime) 
-			{
-				CopyFile(srcFile, dstFile); 
-
-				struct utimbuf time; //Zmieniamy czas modyfikacji pliku przy użyciu funkcji utime
-				time.modtime = srcStats.st_mtime;
-				utime(dstFile ,&time);
-			}
-		}
-		free(dstFile);
-		free(srcFile);
-	}
-    	closedir(srcDIR);
-    	
-	DIR *dstDIR = opendir(destinationDir);
-   	while ((checkAll = readdir(dstDIR)) != NULL) 
-   	{
-   		//Przeszukuje wszystkie pliku pomijając plik . - bieżący katalog oraz .. - nadrzędny
-		if (strcmp(checkAll->d_name, ".") == 0 || strcmp(checkAll->d_name, "..") == 0) 
-		{
-		    continue;
-        	}
-
-        	//Tworzenie scieżek do plików źródłowych i docelowych
-		char* srcFile = malloc((strlen(sourceDir)+strlen(checkAll->d_name)+2)*sizeof(char));
-		strcpy(srcFile, sourceDir);
-		strcat(srcFile, "/");
-		strcat(srcFile, checkAll->d_name);
-		
-		char* dstFile = malloc((strlen(destinationDir)+strlen(checkAll->d_name)+2)*sizeof(char));
-	  	strcpy(dstFile, destinationDir);
-		strcat(dstFile, "/");
-		strcat(dstFile, checkAll->d_name);
-		
-		//Jeżeli plik nie istnieje w katalogu źródłowym to go usuń
-		if (access(srcFile, F_OK) == -1) 
-		{
-
-			printf("%s",srcFile);
-			unlink(dstFile);
-			perror("unlink");
-		}
-		free(srcFile);
-		free(dstFile);
-
-    	}
-    	closedir(dstDIR);
+        switch (argc) {
+            case 3:
+                SynchroniseDirectories(argv[1], argv[2], 0);
+                break;
+            case 4:
+                if (ChangeTime(argv[3])) {
+                    SynchroniseDirectories(argv[1], argv[2], 0);
+                } else if (strcmp(argv[3], "-R") == 0) {
+                    SynchroniseDirectories(argv[1], argv[2], 0);
+                } else {
+                    WriteErrorAtributes(argv[0]);
+                }
+                break;
+            case 5:
+                if (ChangeTime(argv[3])) {
+                } else {
+                    if (strcmp(argv[3], "-R") == 0) {
+                    } else {
+                        WriteErrorAtributes(argv[0]);
+                    }
+                    if (ChangeSize(argv[4])) {
+                        SynchroniseDirectories(argv[1], argv[2], 1);
+                    } else {
+                        WriteErrorAtributes(argv[0]);
+                    }
+                }
+                if (strcmp(argv[4], "-R") == 0) {
+                    SynchroniseDirectories(argv[1], argv[2], 1);
+                } else {
+                    WriteErrorAtributes(argv[0]);
+                }
+                break;
+            case 6:
+                if (ChangeTime(argv[3])) {
+                } else {
+                    WriteErrorAtributes(argv[0]);
+                }
+                if (strcmp(argv[4], "-R") == 0) {
+                } else {
+                    WriteErrorAtributes(argv[0]);
+                }
+                if (ChangeSize(argv[5])) {
+                    SynchroniseDirectories(argv[1], argv[2], 1);
+                } else {
+                    WriteErrorAtributes(argv[0]);
+                }
+            default:
+                break;
+        }
+    } else {
+        printf("Zła liczba parametrów: %s <src_dir> <dest_dir> [sleepTime] [-R]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 }
 
-//Sprawdza czy tablica char jest liczbą
-int ChangeTime(const char* input)
-{
-	for(int i=0; i < strlen(input); i++)
-	{
-		if(!isdigit(input[i]))
-		{
-			return 0;
-		}
-	}
-	sleepTime = atoi(input);
-	return 1;
-}
-int ChangeSize(const char* input)
-{
-	for(int i=0; i < strlen(input); i++)
-	{
-		if(!isdigit(input[i]))
-		{
-			return 0;
-		}
-	}
-	sizeFile = atoi(input);
-	return 1;
-}
-//Sprawdza czy path odnosi się do katalogu
-int IsDirectoryExists(const char *path) 
-{
-	struct stat s;
-	if (stat(path, &s) == -1 || !S_ISDIR(s.st_mode)) 
-		return 1;
-	return 0;
+void CopyFile(const char* srcFile, const char* dstFile) {
+    int fileToRead = open(srcFile, O_RDONLY);
+    struct stat stats;
+    fstat(fileToRead, &stats);
+    
+    if (stats.st_size <= SMALL_FILE_SIZE) {
+        char buf[SMALL_FILE_SIZE];
+        int bytesNumber = read(fileToRead, buf, SMALL_FILE_SIZE);
+        int fileToSave = open(dstFile, O_WRONLY | O_CREAT | O_TRUNC, stats.st_mode);
+        write(fileToSave, buf, bytesNumber);
+        close(fileToSave);
+    } else {
+        void *srcPTR = mmap(NULL, stats.st_size, PROT_READ, MAP_PRIVATE, fileToRead, 0);
+        int fileToSave = open(dstFile, O_WRONLY | O_CREAT | O_TRUNC, stats.st_mode);
+        write(fileToSave, srcPTR, stats.st_size);
+        munmap(srcPTR, stats.st_size);
+        close(fileToSave);
+    }
+    close(fileToRead);
 }
 
-void WriteErrorAtributes(const char *programName)
-{
-	printf("Błąd w parametrach: %s <src_dir> <dest_dir> [sleepTime] [-R] [sizeFile]\n", programName);
-	exit(EXIT_FAILURE);
+void SynchroniseDirectories(const char* sourceDir, const char* destinationDir, int isRecursive) {
+    DIR *srcDIR = opendir(sourceDir);
+    struct dirent *checkAll;
+    while ((checkAll = readdir(srcDIR)) != NULL) {
+        if (strcmp(checkAll->d_name, ".") == 0 || strcmp(checkAll->d_name, "..") == 0) {
+            continue;
+        }
+
+        char* srcFile = malloc((strlen(sourceDir)+strlen(checkAll->d_name)+2)*sizeof(char));
+        strcpy(srcFile, sourceDir);
+        strcat(srcFile, "/");
+        strcat(srcFile, checkAll->d_name);
+        
+        char* dstFile = malloc((strlen(destinationDir)+strlen(checkAll->d_name)+2)*sizeof(char));
+        strcpy(dstFile, destinationDir);
+        strcat(dstFile, "/");
+        strcat(dstFile, checkAll->d_name);
+
+        struct stat srcStats, dstStats;
+        stat(srcFile, &srcStats);
+        
+        if (S_ISDIR(srcStats.st_mode) && isRecursive == 1) {
+            // Implementuj rekurencję -R
+        } else if (S_ISREG(srcStats.st_mode)) {
+            if (stat(dstFile, &dstStats) == -1 || srcStats.st_mtime > dstStats.st_mtime) {
+                CopyFile(srcFile, dstFile); 
+                struct utimbuf time;
+                time.modtime = srcStats.st_mtime;
+                utime(dstFile ,&time);
+            }
+       }
+        free(dstFile);
+        free(srcFile);
+    }
+    closedir(srcDIR);
+    
+    DIR *dstDIR = opendir(destinationDir);
+    while ((checkAll = readdir(dstDIR)) != NULL) {
+        if (strcmp(checkAll->d_name, ".") == 0 || strcmp(checkAll->d_name, "..") == 0) {
+            continue;
+        }
+
+        char* srcFile = malloc((strlen(sourceDir)+strlen(checkAll->d_name)+2)*sizeof(char));
+        strcpy(srcFile, sourceDir);
+        strcat(srcFile, "/");
+        strcat(srcFile, checkAll->d_name);
+        
+        char* dstFile = malloc((strlen(destinationDir)+strlen(checkAll->d_name)+2)*sizeof(char));
+        strcpy(dstFile, destinationDir);
+        strcat(dstFile, "/");
+        strcat(dstFile, checkAll->d_name);
+        
+        if (access(srcFile, F_OK) == -1) {
+            unlink(dstFile);
+            perror("unlink");
+        }
+        free(srcFile);
+        free(dstFile);
+    }
+    closedir(dstDIR);
+}
+
+int ChangeTime(const char* input) {
+    for (int i = 0; i < strlen(input); i++) {
+        if (!isdigit(input[i])) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int ChangeSize(const char* input) {
+    int size = atoi(input);
+    if (size > 0) {
+        return 1;
+    }
+    return 0;
+}
+
+int IsDirectoryExists(const char *path) {
+    struct stat s;
+    if (stat(path, &s) == -1 || !S_ISDIR(s.st_mode)) {
+        return 1;
+    }
+    return 0;
+}
+
+void WriteErrorAtributes(const char *programName) {
+    printf("Błąd w parametrach: %s <src_dir> <dest_dir> [sleepTime] [-R] [sizeFile]\n", programName);
+    exit(EXIT_FAILURE);
 }

afc4eab37c62398643a50352d292c07ac74f4014
InterfectoremCubiculum <eryksliwowskic@gmail.com>
2024-04-06 19:12:01 +0200

    Zmienioną Init (usunięto wywoływanie w niej SynchroniseDirectories, oraz jest troch bardziej czytelnjejszy ), dodano zmienne globalne zapisujące czas spania demona, rozmiar kopiowanych plików oraz opcje rekurencji, dodano także zapis do syslog odnośnie złych parametrów

--- demon.c
+++ demon.c
@@ -18,16 +18,21 @@
 #define STANDARD_SLEEP_TIME 10//300 do testow na razie co 5 sekund zamiast 5 minut
 
 void Init(int argc, char* argv[]);
-void CopyFile(const char* srcFile, const char* dstFile);
-void SynchroniseDirectories(const char* sourceDir, const char* destinationDir, int isRecursive);
+int CopyFile(const char* srcFile, const char* dstFile);
+void SynchroniseDirectories(const char* sourceDir, const char* destinationDir);
 int ChangeTime(const char* input);
 int ChangeSize(const char* input);
 int IsDirectoryExists(const char *path);
 void WriteErrorAtributes(const char *programName);
 void SignalHandler(int sig);
 void AlarmHandler(int sig);
+
 char* sourceDir;
 char* destinationDir;
+int sleepTime = 0; // określa czas spania demona
+int recursion = 0; // 0 - nie rekurecyjne kopiowanie, 1 - rekurecyjne kopiowanie
+int sizeFile = 0; // Przetrzymuje rozmiar pliku ograniczający jaką metodę użyć do kopiowania
+
 int main(int argc, char* argv[])
 {
     openlog("DemonSynchronizujący", LOG_NDELAY, LOG_DAEMON);
@@ -39,7 +44,16 @@ int main(int argc, char* argv[])
     daemon(0, 1);
     // Pobranie pid
     pid_t pid = getpid();
-    alarm(STANDARD_SLEEP_TIME);
+    if(sleepTime ==0)
+    {
+        sleepTime = STANDARD_SLEEP_TIME;
+    }
+    if(sizeFile == 0)
+    {
+        sizeFile = SMALL_FILE_SIZE;
+    }
+    alarm(sleepTime);
+
     closelog();
     // Pętla nieskończona, aby program mógł otrzymywać sygnały
     while (1) {
@@ -53,7 +67,7 @@ void AlarmHandler(int sig) {
         // Wyślij sygnał SIGUSR1
         pid_t pid = getpid();
         kill(pid, SIGUSR1);
-	alarm(STANDARD_SLEEP_TIME);
+	alarm(sleepTime);
 	openlog("DemonSynchronizujący", LOG_NDELAY, LOG_DAEMON);
     	syslog(LOG_INFO,"Uspano Demona.\n");
 	closelog();
@@ -66,104 +80,133 @@ void SignalHandler(int sig) {
         openlog("DemonSynchronizujący", LOG_NDELAY, LOG_DAEMON);
         syslog(LOG_INFO,"Wysłano sygnał SIGUSR1.");
         syslog(LOG_INFO,"Wybudzono Demona.\n");
-        SynchroniseDirectories(sourceDir, destinationDir, 0);
+        SynchroniseDirectories(sourceDir, destinationDir);
 	syslog(LOG_INFO,"Zakończono synchronizację katalogów.\n");
 	closelog();
 	
     }
 }
 
-void Init(int argc, char* argv[]) {
-    if (argc >= 3 && argc <= 6) {
+
+// Inizcjalizacja demona podanymi parametrami
+void Init(int argc, char* argv[]) 
+{
+    if (argc >= 3 && argc <= 6) 
+    {
         sourceDir = argv[1];
         destinationDir = argv[2];
 
-        if (IsDirectoryExists(sourceDir) || IsDirectoryExists(destinationDir)) {
+        if (IsDirectoryExists(sourceDir) || IsDirectoryExists(destinationDir)) 
+        {
             printf("\nNie można otworzyć katalogu/katalogów\n");
             exit(1);
         }
 
-        switch (argc) {
-            case 3:
-                SynchroniseDirectories(argv[1], argv[2], 0);
-                break;
+        switch (argc) 
+        {
             case 4:
-                if (ChangeTime(argv[3])) {
-                    SynchroniseDirectories(argv[1], argv[2], 0);
-                } else if (strcmp(argv[3], "-R") == 0) {
-                    SynchroniseDirectories(argv[1], argv[2], 0);
-                } else {
+                if (ChangeTime(argv[3])) 
+                {} 
+                else if (strcmp(argv[3], "-R") == 0) 
+                {
+                    recursion = 1;
+                }
+                else 
+                {
                     WriteErrorAtributes(argv[0]);
                 }
                 break;
             case 5:
-                if (ChangeTime(argv[3])) {
-                } else {
-                    if (strcmp(argv[3], "-R") == 0) {
-                    } else {
+                if (ChangeTime(argv[3]) ) 
+                {
+                    if (strcmp(argv[4], "-R") == 0) 
+                    {
+                        recursion = 1;
+                    } 
+                    else if(ChangeSize(argv[4]))
+                    {}
+                    else
+                    {
                         WriteErrorAtributes(argv[0]);
                     }
-                    if (ChangeSize(argv[4])) {
-                        SynchroniseDirectories(argv[1], argv[2], 1);
-                    } else {
+                } 
+                else 
+                {
+                    if (strcmp(argv[3], "-R") == 0) 
+                    {
+                        recursion = 1;
+                        if (ChangeSize(argv[4])) 
+                        {} 
+                        else 
+                        {
+                            WriteErrorAtributes(argv[0]);
+                        }
+
+                    }
+                    else 
+                    {
                         WriteErrorAtributes(argv[0]);
                     }
-                }
-                if (strcmp(argv[4], "-R") == 0) {
-                    SynchroniseDirectories(argv[1], argv[2], 1);
-                } else {
-                    WriteErrorAtributes(argv[0]);
                 }
                 break;
             case 6:
-                if (ChangeTime(argv[3])) {
-                } else {
+                if (ChangeTime(argv[3]) && (strcmp(argv[4], "-R") == 0) && (ChangeSize(argv[5])) ) 
+                {} 
+                else 
+                {
                     WriteErrorAtributes(argv[0]);
                 }
-                if (strcmp(argv[4], "-R") == 0) {
-                } else {
-                    WriteErrorAtributes(argv[0]);
-                }
-                if (ChangeSize(argv[5])) {
-                    SynchroniseDirectories(argv[1], argv[2], 1);
-                } else {
-                    WriteErrorAtributes(argv[0]);
-                }
+                break;
             default:
+                WriteErrorAtributes(argv[0]);
                 break;
         }
-    } else {
+    } 
+    else 
+    {
         printf("Zła liczba parametrów: %s <src_dir> <dest_dir> [sleepTime] [-R]\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 }
 
-void CopyFile(const char* srcFile, const char* dstFile) {
+// Kopiuje plik między dwo folderami
+int CopyFile(const char* srcFile, const char* dstFile) 
+{
     int fileToRead = open(srcFile, O_RDONLY);
+    int copyMethod = 0;
     struct stat stats;
     fstat(fileToRead, &stats);
     
-    if (stats.st_size <= SMALL_FILE_SIZE) {
-        char buf[SMALL_FILE_SIZE];
-        int bytesNumber = read(fileToRead, buf, SMALL_FILE_SIZE);
+    if (stats.st_size <= sizeFile) // Metodą read/write, jeżeli plik jest mniejszy  niż parametr sizeFile
+    {
+        char buf[sizeFile];
+        int bytesNumber = read(fileToRead, buf, sizeFile);
         int fileToSave = open(dstFile, O_WRONLY | O_CREAT | O_TRUNC, stats.st_mode);
         write(fileToSave, buf, bytesNumber);
         close(fileToSave);
-    } else {
+        copyMethod = 1;
+    } 
+    else // Metodą mmap/write
+    {
         void *srcPTR = mmap(NULL, stats.st_size, PROT_READ, MAP_PRIVATE, fileToRead, 0);
         int fileToSave = open(dstFile, O_WRONLY | O_CREAT | O_TRUNC, stats.st_mode);
         write(fileToSave, srcPTR, stats.st_size);
         munmap(srcPTR, stats.st_size);
         close(fileToSave);
+        copyMethod = 2;
     }
     close(fileToRead);
+    return copyMethod;
 }
 
-void SynchroniseDirectories(const char* sourceDir, const char* destinationDir, int isRecursive) {
+void SynchroniseDirectories(const char* sourceDir, const char* destinationDir) 
+{
     DIR *srcDIR = opendir(sourceDir);
     struct dirent *checkAll;
-    while ((checkAll = readdir(srcDIR)) != NULL) {
-        if (strcmp(checkAll->d_name, ".") == 0 || strcmp(checkAll->d_name, "..") == 0) {
+    while ((checkAll = readdir(srcDIR)) != NULL) 
+    {
+        if (strcmp(checkAll->d_name, ".") == 0 || strcmp(checkAll->d_name, "..") == 0) 
+        {
             continue;
         }
 
@@ -181,32 +224,47 @@ void SynchroniseDirectories(const char* sourceDir, const char* destinationDir, i
         struct stat srcStats, dstStats;
         stat(srcFile, &srcStats);
         
-        if (S_ISDIR(srcStats.st_mode) && isRecursive == 1) {
+        // Uruchomienie opcji kopiowania rekurecyjnego
+        if (S_ISDIR(srcStats.st_mode) && recursion == 1) 
+        {
             // Implement recursion -R dla Michala
-        } else if (S_ISREG(srcStats.st_mode)) {
-	if (stat(dstFile, &dstStats) == -1 || srcStats.st_mtime > dstStats.st_mtime) {
-    // Wykonuj tylko wtedy, gdy plik źródłowy jest nowszy niż plik docelowy
-    CopyFile(srcFile, dstFile); 
-    struct utimbuf time;
-    time.modtime = srcStats.st_mtime;
-    utime(dstFile ,&time);
-    // Dodanie informacji do logu
-    char logMsg[500];
-    snprintf(logMsg, sizeof(logMsg), "Skopiowano plik %s do %s\n", srcFile, dstFile);
-    syslog(LOG_INFO,"%s",logMsg);
-    
-}
-       }
+        } 
+        else if (S_ISREG(srcStats.st_mode)) 
+        {
+            if (stat(dstFile, &dstStats) == -1 || srcStats.st_mtime > dstStats.st_mtime) 
+            {
+                
+            // Wykonuj tylko wtedy, gdy plik źródłowy jest nowszy niż plik docelowy
+            int copyMethod = CopyFile(srcFile, dstFile); 
+
+            struct utimbuf time;
+            time.modtime = srcStats.st_mtime; 
+            utime(dstFile ,&time); // Zmiana czasu modyfikacji
+
+            // Dodanie informacji do logu
+            char logMsg[500];
+            if(copyMethod == 1) // Metodą read/write dla mniejszych plików
+            {
+                snprintf(logMsg, sizeof(logMsg), "Skopiowano plik %s do %s metodą read/write\n", srcFile, dstFile);
+            }
+            else if(copyMethod == 2) // Metodą mmap/write dla większych plików
+            {
+                snprintf(logMsg, sizeof(logMsg), "Skopiowano plik %s do %s metodą mmap/write \n", srcFile, dstFile);
+            }
+            syslog(LOG_INFO,"%s",logMsg);
+            }
+        }
         free(dstFile);
         free(srcFile);
     }
     closedir(srcDIR);
+    
     DIR *dstDIR = opendir(destinationDir);
     while ((checkAll = readdir(dstDIR)) != NULL) {
         if (strcmp(checkAll->d_name, ".") == 0 || strcmp(checkAll->d_name, "..") == 0) {
             continue;
         }
-
+ 
         char* srcFile = malloc((strlen(sourceDir)+strlen(checkAll->d_name)+2)*sizeof(char));
         strcpy(srcFile, sourceDir);
         strcat(srcFile, "/");
@@ -217,9 +275,10 @@ void SynchroniseDirectories(const char* sourceDir, const char* destinationDir, i
         strcat(dstFile, "/");
         strcat(dstFile, checkAll->d_name);
         
+
+        // Usuwa plik
         if (access(srcFile, F_OK) == -1) {
             unlink(dstFile);
-            //perror("unlink");
             char logMsg[500];
             snprintf(logMsg, sizeof(logMsg), "Usunieto plik %s, poniewaz nie istnieje on już w %s\n", dstFile, srcFile);
             syslog(LOG_INFO,"%s",logMsg);
@@ -230,23 +289,35 @@ void SynchroniseDirectories(const char* sourceDir, const char* destinationDir, i
     closedir(dstDIR);
 }
 
-int ChangeTime(const char* input) {
-    for (int i = 0; i < strlen(input); i++) {
-        if (!isdigit(input[i])) {
+// Sprawdza czy podana wartość jest liczbą i zapisuje jako czas między pobudzeniem demona
+int ChangeTime(const char* input) 
+{
+    for (int i = 0; i < strlen(input); i++) 
+    {
+        if (!isdigit(input[i])) 
+        {
             return 0;
         }
     }
+    sleepTime = atoi(input);
     return 1;
 }
 
-int ChangeSize(const char* input) {
-    int size = atoi(input);
-    if (size > 0) {
-        return 1;
+// Sprawdza czy podana wartość jest liczbą i zapisuje jako nową granice dla metod kopiujących pliki
+int ChangeSize(const char* input) 
+{
+    for (int i = 0; i < strlen(input); i++) 
+    {
+        if (!isdigit(input[i])) 
+        {
+            return 0;
+        }
     }
-    return 0;
+    sizeFile = atoi(input);
+    return 1;
 }
 
+// Sprawdza czy istnieją takie katalogii
 int IsDirectoryExists(const char *path) {
     struct stat s;
     if (stat(path, &s) == -1 || !S_ISDIR(s.st_mode)) {
@@ -255,7 +326,13 @@ int IsDirectoryExists(const char *path) {
     return 0;
 }
 
-void WriteErrorAtributes(const char *programName) {
+
+// Zamyka demona oraz informuje o błędach w podanych parametrach
+void WriteErrorAtributes(const char *programName) 
+{
     printf("Błąd w parametrach: %s <src_dir> <dest_dir> [sleepTime] [-R] [sizeFile]\n", programName);
+    char logMsg[500];
+    snprintf(logMsg, sizeof(logMsg), "Demon został zamknięty z powodu błędnych parametrów");
+    syslog(LOG_INFO,"%s",logMsg);
     exit(EXIT_FAILURE);
 }

61cbeccb4d61966288708d3d02a23414c1cd329f
Mateusz Izdebski <149115843+Maison16@users.noreply.github.com>
2024-04-29 16:44:47 +0200

    dokończenie podziału na pliki

--- demon.c
+++ demon.c
@@ -12,10 +12,7 @@
 #include <signal.h>
 #include <syslog.h>
 
-#define SIGALRM 14
-#define SIGUSR1 10
 #define SMALL_FILE_SIZE (1024*1024)
-#define STANDARD_SLEEP_TIME 10 //300 do testow na razie co 10 sekund zamiast 5 minut
 
 void Init(int argc, char* argv[]);
 int CopyFile(const char* srcFile, const char* dstFile);
@@ -27,39 +24,12 @@ void WriteErrorAttributes(const char *programName);
 void SignalHandler(int sig);
 void AlarmHandler(int sig);
 void RemoveDirectoryRecursively(const char *path, const char *srcFile );
-char* sourceDir;
-char* destinationDir;
-int sleepTime = 0; // określa czas spania demona
-int recursion = 0; // 0 - nie rekurecyjne kopiowanie, 1 - rekurecyjne kopiowanie
-int sizeFile = 0; // Przetrzymuje rozmiar pliku ograniczający jaką metodę użyć do kopiowania
 
-int main(int argc, char* argv[])
-{
-    openlog("DemonSynchronizujący", LOG_PID, LOG_DAEMON);
-    // Ustawienie obsługi sygnałów
-    signal(SIGALRM, AlarmHandler);
-    signal(SIGUSR1, SignalHandler);
-    Init(argc, argv);
-    daemon(0, 1);
-    syslog(LOG_INFO, "Demon Kopiujący Pliki został uruchomiony.\n");
-    // Pobranie pid
-    pid_t pid = getpid();
-    if(sleepTime == 0)
-    {
-        sleepTime = STANDARD_SLEEP_TIME;
-    }
-    if(sizeFile == 0)
-    {
-        sizeFile = SMALL_FILE_SIZE;
-    }
-    alarm(sleepTime);
-    closelog();
-    // Pętla nieskończona, aby program mógł otrzymywać sygnały
-    while (1) {
-         pause();
-    }
-    return 0;
-}
+extern char* sourceDir;
+extern char* destinationDir;
+extern int sleepTime;
+extern int recursion;
+extern int sizeFile;
 
 void AlarmHandler(int sig) {
     // Wyślij sygnał SIGUSR1
@@ -199,7 +169,6 @@ int CopyFile(const char* srcFile, const char* dstFile)
 
 void SynchroniseDirectories(const char* sourceDir, const char* destinationDir) 
 {
-    openlog("DemonSynchronizujący", LOG_PID, LOG_DAEMON);
     DIR *srcDIR = opendir(sourceDir);
     struct dirent *checkAll;
     while ((checkAll = readdir(srcDIR)) != NULL) 
@@ -309,7 +278,6 @@ void SynchroniseDirectories(const char* sourceDir, const char* destinationDir)
         free(dstFile);
     }
     closedir(dstDIR);
-    closelog();
 }
 
 
